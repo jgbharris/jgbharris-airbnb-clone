ddf215d590e40eb03afdfa4eb9fe6d1a028f7891
James Harris <jgbharris@gmail.com>
2024-09-04 12:57:14 +0100

    Listing being created through modal flow and uploaded to DB

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -7,4 +7,41 @@ export async function POST(request: Request) {
   if (!currentUser) {
     return NextResponse.redirect("/login");
   }
+
+  const body = await request.json();
+
+  const {
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+  } = body;
+
+  Object.keys(body).forEach((value: any) => {
+    if (!body[value]) {
+      return NextResponse.error();
+    }
+  });
+
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id,
+    },
+  });
+
+  return NextResponse.json(listing);
 }
