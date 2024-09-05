30258ad634817d3cfd0baeff8ecd1999443de0b4
James Harris <jgbharris@gmail.com>
2024-09-05 14:17:16 +0100

    Adding useFavorite hook into favorites api route - favorite functionality working

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -6,10 +6,7 @@ interface IParams {
   listingId?: string;
 }
 
-export default async function POST(
-  request: Request,
-  { params }: { params: IParams },
-) {
+export async function POST(request: Request, { params }: { params: IParams }) {
   const currentUser = await getCurrentUser();
 
   if (!currentUser) {
