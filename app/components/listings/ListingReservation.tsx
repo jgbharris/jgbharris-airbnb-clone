1c98a795fd6a71a2207120327c193fd941117e62
James Harris <jgbharris@gmail.com>
2024-09-07 20:02:51 +0100

    Calendar on listing page

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Range } from "react-date-range";
+import Calendar from "../inputs/Calendar";
+
 interface ListingReservationProps {
   price: number;
   totalPrice: number;
@@ -10,8 +13,34 @@ interface ListingReservationProps {
   disabledDates: Date[];
 }
 
-const ListingReservation = () => {
-  return <div>Listing Reservation</div>;
+const ListingReservation: React.FC<ListingReservationProps> = ({
+  price,
+  totalPrice,
+  onChangeDate,
+  dateRange,
+  onSubmit,
+  disabled,
+  disabledDates,
+}) => {
+  return (
+    <div className="overflow-hidden rounded-xl border-[1px] border-neutral-200 bg-white">
+      <div className="flex flex-row items-center gap-1 p-4">
+        <div className="text-2xl font-semibold">$ {price}</div>
+        <div className="font-light text-neutral-600">/ night</div>
+      </div>
+      <hr />
+      <Calendar
+        value={dateRange}
+        disabledDates={disabledDates}
+        onChange={(value) => onChangeDate(value.selection)}
+      />
+      <hr />
+      <div className="flex flex-row items-center justify-between p-4 text-lg font-semibold">
+        <div>Total</div>
+        <div>$ {totalPrice}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ListingReservation;
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -103,7 +103,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
   }, [listing.category]);
   return (
     <Container>
-      <div className="mx-auto mt-6 max-w-screen-lg">
+      <div className="mx-auto max-w-screen-lg">
         <div className="flex flex-col gap-6">
           <ListingHead
             title={listing.title}
@@ -112,28 +112,28 @@ const ListingClient: React.FC<ListingClientProps> = ({
             id={listing.id}
             currentUser={currentUser}
           />
-        </div>
-        <div className="mt-6 grid grid-cols-1 md:grid-cols-7 md:gap-10">
-          <ListingInfo
-            user={listing.user}
-            category={category}
-            description={listing.description}
-            roomCount={listing.roomCount}
-            bathroomCount={listing.bathroomCount}
-            guestCount={listing.guestCount}
-            locationValue={listing.locationValue}
-          />
-        </div>
-        <div className="order-first mb-10 md:order-last md:col-span-3">
-          <ListingReservation
-            price={listing.price}
-            totalPrice={totalPrice}
-            onChangeDate={(value) => setDateRange(value)}
-            dateRange={dateRange}
-            onSubmit={onCreateReservation}
-            disabled={isLoading}
-            disabledDates={disabledDates}
-          />
+          <div className="mt-6 grid grid-cols-1 md:grid-cols-7 md:gap-10">
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+            <div className="order-first mb-10 md:order-last md:col-span-3">
+              <ListingReservation
+                price={listing.price}
+                totalPrice={totalPrice}
+                onChangeDate={(value) => setDateRange(value)}
+                dateRange={dateRange}
+                onSubmit={onCreateReservation}
+                disabled={isLoading}
+                disabledDates={disabledDates}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </Container>
